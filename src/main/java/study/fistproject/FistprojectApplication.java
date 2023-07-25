1b3f2027e4e19ef30b260bf8429469cca5b8fca2
Hoo <b01066586906@gamil.com>
2023-07-26 01:32:32 +0900

    Initial commit

---
+++ src/main/java/study/fistproject/FistprojectApplication.java
@@ -0,0 +1,13 @@
+package study.fistproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FistprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FistprojectApplication.class, args);
+	}
+
+}
